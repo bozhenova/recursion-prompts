1b25164d1aa273957b20be96951729b925971b8b
Margo <margo.bozhenova@gmail.com>
2019-12-27 00:29:11 +0300

    Add countKeysInObj

--- src/recursion.js
+++ src/recursion.js
@@ -326,13 +326,24 @@ function timesTwo(elem) {
   return elem * 2;
 }
 
-rMap([1, 2, 3], timesTwo); // [2,4,6]
-
 // 22. Write a function that counts the number of times a key occurs in an object.
-// var obj = {'e':{'x':'y'},'t':{'r':{'e':'r'},'p':{'y':'r'}},'y':'e'};
+// var obj = { 'e': { 'x': 'y' }, 't': { 'r': { 'e': 'r' }, 'p': { 'y': 'r' } }, 'y': 'e' };
 // countKeysInObj(obj, 'r') // 1
 // countKeysInObj(obj, 'e') // 2
-var countKeysInObj = function (obj, key) { };
+
+var countKeysInObj = function (obj, key) {
+  var count = 0;
+  for (var prop in obj) {
+    var item = obj[prop];
+    if (prop === key) {
+      count++;
+    }
+    if (typeof item === 'object') {
+      count += countKeysInObj(item, key);
+    }
+  }
+  return count
+};
 
 // 23. Write a function that counts the number of times a value occurs in an object.
 // var obj = {'e':{'x':'y'},'t':{'r':{'e':'r'},'p':{'y':'r'}},'y':'e'};
