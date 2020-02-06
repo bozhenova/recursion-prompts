327f1f7bc559273a7cb21f6180e0ea2f730d75fc
Margo <margo.bozhenova@gmail.com>
2020-02-06 05:13:18 +0300

    Fix if/else statement

--- src/recursion.js
+++ src/recursion.js
@@ -384,11 +384,11 @@ var fibonacci = function (n) {
   }
   if (n === 1) {
     return [0, 1];
-  } else {
-    var fib = fibonacci(n - 1);
-    fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
-    return fib;
   }
+  var fib = fibonacci(n - 1);
+  fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+  return fib;
+
 };
 
 // 26. Return the Fibonacci number located at index n of the Fibonacci sequence.
